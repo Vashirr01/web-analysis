72f22a216069b4dfb091aa5fa6713eacd357488c
vashirr <130561897+Vashirr01@users.noreply.github.com>
2025-01-28 23:54:48 +0530

    using gemini's api instead of closed ai to test and generate first call

--- test.py
+++ test.py
@@ -1,22 +1,33 @@
-from scraper import WebScraper
+# from scraper import WebScraper
+import os
+from dotenv import load_dotenv
+import google.generativeai as genai
 
-# Example usage
-scraper = WebScraper('https://csprimer.com', delay=2)
+load_dotenv()
+api_key = os.getenv("GEMINI_API")
 
-# Define what data to extract
-selectors = {
-    'title': 'h1',
-    'description': '.description'
-}
+genai.configure(api_key=api_key)
+model = genai.GenerativeModel("gemini-1.5-flash")
+response = model.generate_content("Explain how AI works")
+print(response.text)
 
-# URLs to scrape
-urls = [
-    '/',
-    '/courses/'
-]
-
-# Scrape the pages
-data = scraper.scrape_pages(urls, selectors)
-
-# Save results
-scraper.save_to_csv(data, 'scraped_data.csv')
+# # Example usage
+# scraper = WebScraper('https://www.mathacademy.com', delay=2)
+#
+# # Define what data to extract
+# selectors = {
+#     'title': 'h1',
+#     'description': '.description',
+#     'paragraphs' : 'p'
+# }
+#
+# # URLs to scrape
+# urls = [
+#     '/',
+# ]
+#
+# # Scrape the pages
+# data = scraper.scrape_pages(urls, selectors)
+#
+# # Save results
+# scraper.save_to_csv(data, 'scraped_data.csv')
